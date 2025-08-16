4d5b5e55de9b8afef92eabca001edb63a3de9c56
Dakshesh Bijawar <bdaksheshbijawar735@gmail.com>
2025-08-17 00:11:56 +0530

    basic project working

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
+import dotenv from "dotenv";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import db from "./src/config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+
+app.get("/", (req, res) => {
+  res.send("Music Studio Inventory App is running with ES Modules!");
+});
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await db.raw("SELECT NOW()");
+    res.json({ success: true, time: result.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, error: "DB connection failed" });
+  }
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
